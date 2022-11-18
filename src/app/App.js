a8da1179b904c3973c1426d6c3a697818b2fad84
Nikita Demyanenko <nikitademyanenko000@gmail.com>
2022-11-18 17:54:34 +0300

    delete styled-components package and install sass

--- src/app/App.js
+++ src/app/App.js
@@ -1,24 +1,10 @@
 import React from "react";
 import { useRoutes } from "react-router-dom";
-import { createGlobalStyle } from "styled-components";
 import routes from "./routes";
-
-const Global = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`;
 
 function App() {
     const elements = useRoutes(routes);
-    return (
-        <>
-            <Global />
-            {elements}
-        </>
-    );
+    return <>{elements}</>;
 }
 
 export default App;
