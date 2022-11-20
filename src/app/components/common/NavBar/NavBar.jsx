7104d8381f27f4cec8ba2bc358f141664ac972fd
Nikita Demyanenko <nikitademyanenko000@gmail.com>
2022-11-20 06:18:45 +0300

    resolved branch conflicts

--- src/app/components/common/NavBar/NavBar.jsx
+++ src/app/components/common/NavBar/NavBar.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./styles/navbar.module.scss";
 import Logo from "../Logo";
-import Container from "../Container";
+// import burgerMenuIcon from "../../../assets/svg/menu.svg";
+import PhoneMenu from "../../ui/PhoneMenu/PhoneMenu";
 
 const NavBar = () => {
-    
+    const [openMenu, setOpen] = useState(false);
     const navLinks = [
         { path: "/", name: "Команда", id: 1 },
         {
@@ -14,34 +15,46 @@ const NavBar = () => {
             id: 2
         }
     ];
+    const handleOpenMenu = () => {
+        setOpen((prevState) => !prevState);
+    };
     return (
         <header className={styles.header}>
-            <Container>
-                <div className={styles.header__body}>
-                    <Logo />
-                    <nav className={styles.header__nav}>
-                        <ul className={styles.header__nav_list}>
-                            {navLinks.map((link) => (
-                                <li
-                                    key={link.id}
-                                    className={styles.header__nav_item}
+            <div className={styles.header__container}>
+                <Logo />
+                <nav className={styles.header__nav}>
+                    <ul className={styles.header__nav_list}>
+                        {navLinks.map((link) => (
+                            <li
+                                key={link.id}
+                                className={styles.header__nav_item}
+                            >
+                                <NavLink
+                                    className={styles.header__nav_link}
+                                    to={link.path}
                                 >
-                                    <NavLink
-                                        className={styles.header__nav_link}
-                                        to={link.path}
-                                    >
-                                        {link.name}
-                                    </NavLink>
-                                </li>
-                            ))}
-                        </ul>
-                    </nav>
-                </div>
-                <PhoneMenu links={navLinks} open={openMenu} />
-            </Container>
+                                    {link.name}
+                                </NavLink>
+                            </li>
+                        ))}
+                    </ul>
+                    <input
+                        id="menu__toggle"
+                        className={styles.header__menu_toggle}
+                        type="checkbox"
+                        onClick={handleOpenMenu}
+                    />
+                    <label
+                        className={styles.header__menu_button}
+                        htmlFor="menu__toggle"
+                    >
+                        <span></span>
+                    </label>
+                </nav>
+            </div>
+            <PhoneMenu links={navLinks} open={openMenu} />
         </header>
     );
 };
 
 export default NavBar;
-
