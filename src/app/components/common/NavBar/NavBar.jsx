b7bb604f1c5e2ab781f4be0f3d230d53a8546c9d
AntonKulagin <i-gorusich@mail.ru>
2022-11-20 18:48:41 +0300

    corrected header makeup

--- src/app/components/common/NavBar/NavBar.jsx
+++ src/app/components/common/NavBar/NavBar.jsx
@@ -3,15 +3,17 @@ import { NavLink, useNavigate } from "react-router-dom";
 import styles from "./styles/navbar.module.scss";
 import Logo from "../Logo";
 import PhoneMenu from "../../ui/PhoneMenu/PhoneMenu";
-import { useSelector } from "react-redux";
-import { getCurrentUser } from "../../../store/slices/auth";
+import { useDispatch, useSelector } from "react-redux";
+import { getCurrentUser, logout } from "../../../store/slices/auth";
 import Avatar from "../Avatar";
 import Button from "../Button";
+import Container from "../Container";
 
 const NavBar = () => {
     const [openMenu, setOpen] = useState(false);
     const currentUser = useSelector(getCurrentUser());
     const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     const navLinks = [
         { path: "/", name: "Команда", id: 1 },
@@ -22,53 +24,67 @@ const NavBar = () => {
         }
     ];
 
+    const handleButton = () => {
+        if (!currentUser) navigate("/login/signin");
+        else dispatch(logout());
+    };
+
     const handleOpenMenu = () => {
         setOpen((prevState) => !prevState);
     };
 
     return (
         <header className={styles.header}>
-            <div className={styles.header__container}>
-                <Logo />
-                <div className={styles.header__nav}>
-                    <ul className={styles.header__nav_list}>
-                        {navLinks.map((link) => (
-                            <li
-                                key={link.id}
-                                className={styles.header__nav_item}
-                            >
-                                <NavLink
-                                    className={styles.header__nav_link}
-                                    to={link.path}
+            <Container>
+                <div className={styles.header__container}>
+                    <Logo />
+                    <div className={styles.header__nav}>
+                        <ul className={styles.header__nav_list}>
+                            {navLinks.map((link) => (
+                                <li
+                                    key={link.id}
+                                    className={styles.header__nav_item}
                                 >
-                                    {link.name}
-                                </NavLink>
-                            </li>
-                        ))}
-                    </ul>
-                    <input
-                        id="menu__toggle"
-                        className={styles.header__menu_toggle}
-                        type="checkbox"
-                        onClick={handleOpenMenu}
-                    />
-                    <label
-                        className={styles.header__menu_button}
-                        htmlFor="menu__toggle"
-                    >
-                        <span></span>
-                    </label>
+                                    <NavLink
+                                        className={styles.header__nav_link}
+                                        to={link.path}
+                                    >
+                                        {link.name}
+                                    </NavLink>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className={styles.header__right}>
+                        <div className={styles.header__avatar}>
+                            {currentUser && (
+                                <Avatar src={currentUser.image} size="45" />
+                            )}
+                        </div>
+                        <div className={styles.header__logButton}>
+                            <Button onClick={handleButton}>
+                                {currentUser ? "Выход" : "Вход"}
+                            </Button>
+                        </div>
+                        <div className={styles.header__iconMenu}>
+                            <input
+                                id="menu__toggle"
+                                className={styles.header__menu_toggle}
+                                type="checkbox"
+                                onClick={handleOpenMenu}
+                            />
+                            <label
+                                className={styles.header__menu_button}
+                                htmlFor="menu__toggle"
+                            >
+                                <span></span>
+                            </label>
+                        </div>
+                    </div>
                 </div>
-                {!currentUser ? (
-                    <Button onClick={() => navigate("/login/signin")}>
-                        Вход
-                    </Button>
-                ) : (
-                    <Avatar src={currentUser.image} size="45" />
-                )}
-            </div>
 
-            <PhoneMenu links={navLinks} open={openMenu} />
+                <PhoneMenu links={navLinks} open={openMenu} />
+            </Container>
         </header>
     );
 };
