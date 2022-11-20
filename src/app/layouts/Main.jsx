1092358f820dbd8e6d1a9edc9d511978d9734efb
AlexKoshelev <2391382@mail.ru>
2022-11-20 19:36:45 +0300

    breadcrumbs

--- src/app/layouts/Main.jsx
+++ src/app/layouts/Main.jsx
@@ -3,12 +3,15 @@ import Navbar from "../components/common/NavBar/NavBar";
 import MainPage from "../components/pages/MainPage";
 import Container from "../components/common/Container";
 import Footer from "../components/common/Footer";
+import BreadCrumb from "../components/common/breadCrumbs/breadCrumbs";
 
 const Main = () => {
     return (
         <>
             <Navbar />
+
             <Container>
+                <BreadCrumb />
                 <MainPage />
             </Container>
             <Footer />
